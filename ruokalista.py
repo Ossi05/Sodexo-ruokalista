201d248cb35c336ee3687b3bc7330c0eb8839b0c
Ossi <77546709+Ossi05@users.noreply.github.com>
2023-03-13 10:38:26 +0200

    Update ruokalista.py

--- ruokalista.py
+++ ruokalista.py
@@ -2,6 +2,7 @@ import feedparser
 import datetime
 from bs4 import BeautifulSoup
 
+#rss feed linkki
 url = "https://www.sodexo.fi/ruokalistat/rss/weekly_rss/115/fi"
 feed = feedparser.parse(url)
 
@@ -14,11 +15,12 @@ for entry in feed.entries:
     soup = BeautifulSoup(summary, 'html.parser')
     strong_tag = soup.find('strong')
     day_tag = soup.find('h2')
+    #Tänään
     if strong_tag and day_tag and day_tag.text == days[today]:
         print(f"{days[today]}:")
         print(f"          {strong_tag.text}")
         print("Muut päivat:")
-        
+    #Muut päivät    
     else:
         if strong_tag:
             print(f"          {strong_tag.text}")
