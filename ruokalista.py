cbf93a40ce2ea706104b3641fd14a2de4b127a53
Ossi <77546709+Ossi05@users.noreply.github.com>
2023-03-13 10:35:34 +0200

    Add files via upload

---
+++ ruokalista.py
@@ -0,0 +1,24 @@
+import feedparser
+import datetime
+from bs4 import BeautifulSoup
+
+url = "https://www.sodexo.fi/ruokalistat/rss/weekly_rss/115/fi"
+feed = feedparser.parse(url)
+
+today = datetime.datetime.today().weekday()
+days = ["Maanantai", "Tiistai", "Keskiviikko", "Torstai", "Perjantai"]
+print(feed.feed.description + ":")
+
+for entry in feed.entries:
+    summary = entry.summary_detail.value
+    soup = BeautifulSoup(summary, 'html.parser')
+    strong_tag = soup.find('strong')
+    day_tag = soup.find('h2')
+    if strong_tag and day_tag and day_tag.text == days[today]:
+        print(f"{days[today]}:")
+        print(f"          {strong_tag.text}")
+        print("Muut päivat:")
+        
+    else:
+        if strong_tag:
+            print(f"          {strong_tag.text}")
